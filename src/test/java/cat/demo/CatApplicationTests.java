f94d452e3369c4ba99d5dd041549db1a9b4ae2bc
Siming Jia <64111362+nomekop65@users.noreply.github.com>
2025-05-31 14:32:09 -0400

    change config file

---
+++ src/test/java/cat/demo/CatApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
